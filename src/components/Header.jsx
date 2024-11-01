2cdd6e76ae1731e3388c592907f21d5bef01bbe2
Biniyam Belay <119636505+Biniyam-Belay@users.noreply.github.com>
2024-11-01 20:31:56 +0000

    Edited the services, and  collaboration sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,7 +35,7 @@ const Header = () => {
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
       }`}
     >
-      <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
+      <div className="flex items-center ml-auto px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
         <a className="block w-[12rem] xl:mr-8" href="#hero">
           <img src={brainwave} width={190} height={40} alt="Brainwave" />
         </a>
@@ -43,9 +43,9 @@ const Header = () => {
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:ml-auto lg:bg-transparent`}
         >
-          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
+          <div className="relative z-2 flex flex-col items-center justify-stretch lg:flex-row">
             {navigation.map((item) => (
               <a
                 key={item.id}
@@ -65,16 +65,6 @@ const Header = () => {
           </div>
           <HamburgerMenu />
         </nav>
-
-        <a
-          href="#signup"
-          className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
-        >
-          New account
-        </a>
-        <Button className="hidden lg:flex" href="#login">
-          Sign in
-        </Button>
 
         <Button
           className="ml-auto lg:hidden"
